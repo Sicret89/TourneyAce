4cbad58d7ae4f8d9ffeb574bd080767ff282b7d5
Kamil-Banas <k_banas@goodyear.com>
2025-04-03 10:05:24 +0200

    fix routers in main app

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
-# from routes import players
-from auth.auth_router import router as auth_router
 from database import init_db
+from auth.auth_router import router as auth_router
 from routes.players import router as player_router
 
 from contextlib import asynccontextmanager
@@ -13,9 +12,8 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Poker Tournament Manager", lifespan=lifespan)
 
-# app.include_router(players.router, prefix="/players", tags=["Players"])
-app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
-app.include_router(player_router, prefix="/players", tags=["Players"])
+app.include_router(auth_router)
+app.include_router(player_router)
 
 @app.get("/")
 def read_root():
